7fcd6221076346b6296d2444f42a7054353c6fe0
vkalenska <vkalenska@codix.bg>
2021-07-02 12:07:26 +0300

    commit

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { UsersService } from './users.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {UsersService} from './users.service';
 
 @Component({
   selector: 'app-users',
@@ -10,25 +10,25 @@ export class UsersComponent implements OnInit {
   users: any;
   posts: any;
   isReady = false;
-  
 
-  constructor(private usersSvc: UsersService) { }
+
+  constructor(private usersSvc: UsersService) {
+  }
 
   ngOnInit(): void {
     this.usersSvc.getAllUsers().subscribe(res => {
       this.users = res;
       this.isReady = true;
     })
-   
 
-    
+
   }
 
 
   getPosts(userId: any) {
     this.usersSvc.getPostsByUser(userId).subscribe(posts => {
       this.posts = posts;
-      
-     });
+
+    });
   }
 }
